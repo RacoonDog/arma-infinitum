0c1368ad16c3a058cee04aae7205bc1ab3d13bbb
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2021-09-17 23:14:49 -0400

    Fixed issue #2
    + updated dependencies

---
+++ src/main/java/io/github/racoondog/arma_infinitum/mixin/ProjectileEntityMixin.java
@@ -0,0 +1,25 @@
+package io.github.racoondog.arma_infinitum.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.util.hit.EntityHitResult;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ProjectileEntity.class)
+public abstract class ProjectileEntityMixin {
+    @Inject(method = "onEntityHit", at = @At("HEAD"), cancellable = true)
+    protected void onEntityHit(EntityHitResult entityHitResult, CallbackInfo info) {
+        if (!(((ProjectileEntity) (Object) this) instanceof PersistentProjectileEntity)) {
+            Entity entity = entityHitResult.getEntity();
+            boolean bl = entity.getType() == EntityType.ENDERMAN;
+            if (((ProjectileEntity) (Object) this).isOnFire() && !bl) {
+                entity.setOnFireFor(5);
+            }
+        }
+    }
+}
