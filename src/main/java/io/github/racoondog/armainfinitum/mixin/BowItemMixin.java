fde873fd9f8d9d0f35c1f8358e0e5aa4e90e603c
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2021-11-30 20:10:58 -0500

    new bow mixin

--- src/main/java/io/github/racoondog/armainfinitum/mixin/BowItemMixin.java
+++ src/main/java/io/github/racoondog/armainfinitum/mixin/BowItemMixin.java
@@ -1,5 +1,8 @@
 package io.github.racoondog.armainfinitum.mixin;
 
+import io.github.racoondog.armainfinitum.ArmaInfinitum;
+import io.github.racoondog.armainfinitum.util.BowUtil;
+import io.github.racoondog.armainfinitum.util.ThrowItemUtil;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.LivingEntity;
@@ -15,68 +18,30 @@ import net.minecraft.stat.Stats;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Random;
 
 @Mixin(BowItem.class)
 public abstract class BowItemMixin {
-    /**
-     * @author crosby
-     * @reason
-     */
-    @Overwrite
-    public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {
-        if (user instanceof PlayerEntity playerEntity) {
-            boolean bl = playerEntity.getAbilities().creativeMode || EnchantmentHelper.getLevel(Enchantments.INFINITY, stack) > 0;
-            ItemStack itemStack = playerEntity.getArrowType(stack);
-            if (!itemStack.isEmpty() || bl) {
-                if (itemStack.isEmpty()) {
-                    itemStack = new ItemStack(Items.ARROW);
-                }
-
-                int i = stack.getItem().getMaxUseTime(stack) - remainingUseTicks;
-                float f = BowItem.getPullProgress(i);
-                if (!((double)f < 0.1D)) {
-                    boolean bl2 = bl && itemStack.isOf(Items.ARROW);
-                    if (!world.isClient) {
-                        ArrowItem arrowItem = (ArrowItem)(itemStack.getItem() instanceof ArrowItem ? itemStack.getItem() : Items.ARROW);
-                        PersistentProjectileEntity persistentProjectileEntity = arrowItem.createArrow(world, itemStack, playerEntity);
-                        persistentProjectileEntity.setProperties(playerEntity, playerEntity.getPitch(), playerEntity.getYaw(), 0.0F, f * 3.0F, 1.0F);
-                        if (f == 1.0F) {
-                            persistentProjectileEntity.setCritical(true);
-                        }
-
-                        int j = EnchantmentHelper.getLevel(Enchantments.POWER, stack);
-                        if (j > 0) {
-                            persistentProjectileEntity.setDamage(persistentProjectileEntity.getDamage() + (double)j * 0.5D + 0.5D);
-                        }
-
-                        int k = EnchantmentHelper.getLevel(Enchantments.PUNCH, stack);
-                        if (k > 0) {
-                            persistentProjectileEntity.setPunch(k);
-                        }
-
-                        if (EnchantmentHelper.getLevel(Enchantments.FLAME, stack) > 0) {
-                            persistentProjectileEntity.setOnFireFor(100);
-                        }
-
-                        //stack.damage(1, (LivingEntity)playerEntity, (Consumer)((p) -> {
-                        //    p.sendToolBreakStatus(playerEntity.getActiveHand());
-                        //}));
-                        if (bl2 || playerEntity.getAbilities().creativeMode && (itemStack.isOf(Items.SPECTRAL_ARROW) || itemStack.isOf(Items.TIPPED_ARROW))) {
-                            persistentProjectileEntity.pickupType = PersistentProjectileEntity.PickupPermission.CREATIVE_ONLY;
-                        }
-
-                        world.spawnEntity(persistentProjectileEntity);
-                    }
-
-                    world.playSound((PlayerEntity)null, playerEntity.getX(), playerEntity.getY(), playerEntity.getZ(), SoundEvents.ENTITY_ARROW_SHOOT, SoundCategory.PLAYERS, 1.0F, 1.0F / (world.getRandom().nextFloat() * 0.4F + 1.2F) + f * 0.5F);
-                    if (!bl2 && !playerEntity.getAbilities().creativeMode) {
-                        itemStack.decrement(1);
-                        if (itemStack.isEmpty()) {
-                            playerEntity.getInventory().removeOne(itemStack);
-                        }
-                    }
-
-                    playerEntity.incrementStat(Stats.USED.getOrCreateStat(stack.getItem()));
+    @Inject(method = "onStoppedUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;spawnEntity(Lnet/minecraft/entity/Entity;)Z", shift = At.Shift.AFTER))
+    private void multishot(ItemStack stack, World world, LivingEntity user, int remainingUseTicks, CallbackInfo ci) {
+        int level = EnchantmentHelper.getLevel(Enchantments.MULTISHOT, stack);
+        if (level > 0) {
+            int count = ThrowItemUtil.countProjectiles(level) - 1;
+            Random random = user.getRandom();
+            int spread = world.getGameRules().getInt(ArmaInfinitum.PROJECTILE_SPREAD);
+            float f = BowItem.getPullProgress(stack.getItem().getMaxUseTime(stack) - remainingUseTicks);
+            for (int i = 0; i < count; i++) {
+                if (world.getGameRules().getBoolean(ArmaInfinitum.BOW_MULTISHOT_RANDOM)) {
+                    //Random
+                    BowUtil.shoot(world, user, stack, ThrowItemUtil.genRandom(user.getPitch(), random, spread), ThrowItemUtil.genRandom(user.getYaw(), random, spread), f);
+                } else {
+                    //Not Random
+                    //Temp
+                    BowUtil.shoot(world, user, stack, ThrowItemUtil.genRandom(user.getPitch(), random, spread), ThrowItemUtil.genRandom(user.getYaw(), random, spread), f);
                 }
             }
         }
---
+++ src/main/java/io/github/racoondog/armainfinitum/util/BowUtil.java
@@ -0,0 +1,35 @@
+package io.github.racoondog.armainfinitum.util;
+
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.item.ArrowItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.world.World;
+
+public class BowUtil {
+    public static void shoot(World world, LivingEntity user, ItemStack stack, float pitch, float yaw, float pullProgress) {
+        ArrowItem arrowItem = (ArrowItem)(stack.getItem() instanceof ArrowItem ? stack.getItem() : Items.ARROW);
+        PersistentProjectileEntity persistentProjectileEntity = arrowItem.createArrow(world, stack, user);
+        persistentProjectileEntity.setProperties(user, pitch, yaw, 0.0f, pullProgress * 3.0f, 1.0f);
+
+        if (pullProgress == 1.0f) {
+            persistentProjectileEntity.setCritical(true);
+        }
+        int power;
+        if ((power = EnchantmentHelper.getLevel(Enchantments.POWER, stack)) > 0) {
+            persistentProjectileEntity.setDamage(persistentProjectileEntity.getDamage() + (double)power * 0.5 + 0.5);
+        }
+        int punch;
+        if ((punch = EnchantmentHelper.getLevel(Enchantments.PUNCH, stack)) > 0) {
+            persistentProjectileEntity.setPunch(punch);
+        }
+        if (EnchantmentHelper.getLevel(Enchantments.FLAME, stack) > 0) {
+            persistentProjectileEntity.setOnFireFor(100);
+        }
+
+        world.spawnEntity(persistentProjectileEntity);
+    }
+}
